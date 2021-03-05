96b2704a9404898cfa21b2a8af75af88b87fb406
korolevskiy08 <emkorolev12@gmail.com>
2021-03-05 18:04:55 +0300

    feat: fixed the loop

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,11 +3,12 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(matrix) {
   let count = 0;
   for ( let i = 0; i < matrix.length; i++){        // внешний цикл
-    for( let i = 0; i < matrix[i]; i++){
-      if ( matrix[i] === '^^' ){count++};
+    for( let x = 0; x < matrix[i].length; x++){
+      if ( matrix[i][x] === '^^' ){count++};
     }
+    
   } 
-  throw new CustomError('Not implemented');
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 return count;
 };
