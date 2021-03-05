21c2640a429c0ddb3f4157c6632d6cf7258e2f89
korolevskiy08 <emkorolev12@gmail.com>
2021-03-05 20:34:22 +0300

    feat: done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,17 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(arr) {
-  let arr1 = [];
+module.exports = function transform(inArr) {
+  let arr =inArr.slice() 
+  if(arr.length ==0 ){
+    return arr
+  }
   for (let i = 0; i < arr.length; i++){
-    if ( arr.length == 0){}
-    if (!(arr1 instanceof Array)){throw new Error}
-    if ( arr[i] == `--discard-next`){arr1.pop(arr[i + 1])}
-    if ( arr[i] == `--discard-prev`){arr1.pop(arr[i - 1])}
-    if ( arr[i] == `--double-next` ){arr1[i + 1]*2 }
-    if ( arr[i] == `--double-prev` ){arr1[i - 1]*2 }
-  } 
-  
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-
+    if ( arr[i] == `--discard-next`){
+      arr.splice(i,2,'delete this','delete this');
+      if(i == arr.length-1) {
+        arr[i] = 'delete this';
+      }
+    };
+    if ( arr[i] == '--discard-prev'){
+      if(i == 0) {
+        arr[i] = 'delete this';
+      }else{
+        arr.splice(i - 1,2,'delete this','delete this');
+      }
+    }
+    if ( arr[i] == `--double-next`){
+      if(i == arr.length-1) {
+        arr[i] = 'delete this';
+      }else{
+      arr[i] = arr[i + 1]
+      }
+    }
+    if ( arr[i] == `--double-prev`){
+      arr[i] = arr[i - 1]
+      if (i == 0){
+        arr[0] = 'delete this';
+      }
+    }
+  }
+  return arr.filter(a=>a!='delete this')
 };
